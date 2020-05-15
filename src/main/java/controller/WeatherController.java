cef096200e538aafc2fcec41a2212b65407812f0
Jan Krupa <jan.krupa@vsb.cz>
2020-05-15 17:28:10 +0200

    Use PathVariable (city) from URL and render it's temperature

--- src/main/java/controller/WeatherController.java
+++ src/main/java/controller/WeatherController.java
@@ -6,20 +6,24 @@ import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import domain.AWeather;
 import service.WeatherService;
 
+@RequestMapping("/weather")
 @Controller
 public class WeatherController {
-
 	@Autowired
 	private WeatherService weatherService;
 
-	@RequestMapping("/weather/city")
-	public void weather(Model model) throws JSONException, IOException {
-		AWeather weather = weatherService.getWeatherByCity("Opava");
+	@RequestMapping(value = "/{cityName}", method = RequestMethod.GET)
+	public String weather(Model model, @PathVariable String cityName) throws JSONException, IOException {
+		AWeather weather = weatherService.getWeatherByCity(cityName);
 		model.addAttribute("weather", weather);
+		return "/weather/city";
 	}
+
 }

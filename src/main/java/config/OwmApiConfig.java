702c7a378e59a16e16358ef989cbdd27176f661a
Jan Krupa <jan.krupa@vsb.cz>
2020-05-18 07:59:52 +0200

    Load api key from properties file

--- src/main/java/config/OwmApiConfig.java
+++ src/main/java/config/OwmApiConfig.java
@@ -1,7 +1,21 @@
 package config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
 public class OwmApiConfig {
-	static private String OwmApiKey = "************API KEY***********";
+	public static String OwmApiKey;
+
+	@Value("${my.owmkey}")
+	public void setOwmApiKey(String api_key) {
+		OwmApiConfig.OwmApiKey = api_key;
+	}
+
+	public OwmApiConfig() {
+	}
 
 	public static String getApiKey() {
 		return OwmApiKey;
